a4fc6fb58677da3218ed5e6a5c741073d8718ca5
Ravindra <22148496+RSCipher001@users.noreply.github.com>
2021-10-08 23:54:27 +0530

    Vue state is persistent on refresh

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import Vuex from "vuex";
 import auth from "./modules/auth.state";
 import project from "./modules/project.state";
+import createPersistedState from "vuex-persistedstate";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
@@ -9,4 +10,5 @@ export default new Vuex.Store({
   mutations: {},
   actions: {},
   modules: { auth, project },
+  plugins: [createPersistedState()],
 });
