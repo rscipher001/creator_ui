e8bf5938d06b69f29361d9f4305eab880565cf4b
Captain Snowball <22148496+SecureSnowball@users.noreply.github.com>
2021-12-12 17:31:27 +0530

    Home page is now card based

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,35 +1,45 @@
 <template>
-  <section>
-    <div
-      class="container is-max-desktop has-text-centered"
-      style="margin-top: 72px"
-    >
-      <h1 class="is-size-4 has-text-weight-bold has-text-primary">
-        Eliminate boring parts of software development
-      </h1>
-      <p class="mt-2">
-        Tired of writing same auth, CRUD, over and over? Generate boring parts
-        of your software and save time and money
-      </p>
-      <div class="mb-6 mt-3 buttons is-centered">
-        <template v-if="!user">
-          <b-button tag="router-link" to="/register" type="is-primary"
-            >Try now</b-button
+  <section class="container">
+    <div class="section">
+      <div class="columns box">
+        <div class="column">
+          <div
+            class="container is-max-desktop has-text-centered"
+            style="margin-top: 72px"
           >
-          <b-button tag="router-link" to="/login" type="is-secondary"
-            >Login</b-button
-          >
-        </template>
-        <b-button v-else tag="router-link" to="/dashboard" type="is-primary"
-          >Dashboard</b-button
-        >
-      </div>
-      <div>
-        <img
-          style="max-width: 480px"
-          src="@/assets/programming.svg"
-          alt="Programmer with code illustration"
-        />
+            <h1 class="is-size-4 has-text-weight-bold has-text-primary">
+              Eliminate boring parts of software development
+            </h1>
+            <p class="mt-2">
+              Tired of writing same auth, CRUD, over and over? Generate boring
+              parts of your software and save time and money
+            </p>
+            <div class="mb-6 mt-5 buttons is-centered">
+              <template v-if="!user">
+                <b-button tag="router-link" to="/register" type="is-primary"
+                  >Try now</b-button
+                >
+                <b-button tag="router-link" to="/login" type="is-secondary"
+                  >Login</b-button
+                >
+              </template>
+              <b-button
+                v-else
+                tag="router-link"
+                to="/dashboard"
+                type="is-primary"
+                >Dashboard</b-button
+              >
+            </div>
+            <div>
+              <img
+                style="max-width: 480px"
+                src="@/assets/programming.svg"
+                alt="Programmer with code illustration"
+              />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </section>
