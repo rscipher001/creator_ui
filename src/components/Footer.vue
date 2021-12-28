900683567c379d7ecf1b0cc6d220d06844d25377
Captain Snowball <22148496+SecureSnowball@users.noreply.github.com>
2021-12-29 00:07:57 +0530

    Pagination is option

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,21 +1,25 @@
 <template>
-  <footer class="footer">
-    <div class="content has-text-centered">
-      <p>
-        <strong>Website</strong> by
-        <a target="_blank" href="https://github.com/RSCipher001"
-          >Captain Snowball</a
-        >
-      </p>
-      <div>
-        Logo Icon made by
-        <a href="https://www.flaticon.com/authors/becris" title="Becris"
-          >Becris</a
-        >
-        from
-        <a href="https://www.flaticon.com/" title="Flaticon"
-          >www.flaticon.com</a
-        >
+  <footer class="section py-10">
+    <div class="columns">
+      <div class="column px-0">
+        <div class="has-text-centered">
+          <p>
+            <strong>Website</strong> by
+            <a target="_blank" href="https://github.com/RSCipher001">
+              Captain Snowball
+            </a>
+          </p>
+          <div>
+            Logo Icon made by
+            <a href="https://www.flaticon.com/authors/becris" title="Becris">
+              Becris
+            </a>
+            from
+            <a href="https://www.flaticon.com/" title="Flaticon">
+              www.flaticon.com
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   </footer>
