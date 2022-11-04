f5cff41f00deaeb0823b644b1fbd50e24598e251
Captain Snowball <22148496+SecureSnowball@users.noreply.github.com>
2022-11-04 14:38:47 +0530

    Env file support added in vite

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,23 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import { createVuePlugin as vue } from "vite-plugin-vue2";
 const path = require("path");
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    extensions: [".mjs", ".js", ".ts", ".jsx", ".tsx", ".json", ".vue"],
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ _, mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    server: {
+      port: env.PORT,
     },
-  },
+    preview: {
+      port: env.PORT,
+    },
+    plugins: [vue()],
+    resolve: {
+      extensions: [".mjs", ".js", ".ts", ".jsx", ".tsx", ".json", ".vue"],
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+  };
 });
